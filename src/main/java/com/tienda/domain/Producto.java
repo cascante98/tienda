fdb505c2eb9e8f841b2258265879b9322494b8e0
cascante98 <cascante_98@outlook.com>
2025-03-24 21:00:22 -0600

    Commit Sem 11

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -55,5 +55,16 @@ public long getIdProducto() {
         return rutaImagen;
     }
     
+      private int cantidad; // Asegúrate de que este campo exista
+
+    // Getters y setters
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
 
 }
